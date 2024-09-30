a42b8f31d0a2f4f057030da384cc4ac3bb6d7154
Benson-Zheng-ICD2O1-02 <benson.zheng3@student.tdsb.on.ca>
2024-09-30 17:46:10 +0000

    epowpeodw

--- PP1_9.py
+++ PP1_9.py
@@ -2,30 +2,39 @@
 
 def q1(): 
   #Write Assignment code here
-
+  word = "Hello World"
+  print(f"\"{word}\"")
 
 def q2(): 
   #Write Assignment code here
+  user_input = input("Input a word: ")
+  print(user_input.lower())
+  print(user_input.upper())
 
 
 def q3(): 
   #Write Assignment code here
+  user_input = input("Input a word that is at least 5 letters long: ")
+  print(user_input[1:4])
 
 
 def q4(): 
   #Write Assignment code here
-
+  user_input = input("Input a word: ")
+  print(user_input.index("o"))
 
 def q5(): 
   #Write Assignment code here
+  user_input = input("Input a word: ")
+  print(len(user_input))
 
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
